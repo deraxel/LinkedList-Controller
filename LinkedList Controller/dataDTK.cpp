466d8508caf688824b5b2fa29465c11e4c67c3b0
Jason <deraxel.noreply@github.com>
2017-05-22 10:20:10 -0400

    init/temp data class

---
+++ LinkedList Controller/dataDTK.cpp
@@ -0,0 +1,9 @@
+#include. "dtatDTK.h"
+
+int DataDTK::getItem1() const{
+	return item;
+}
+
+void DataDTK::setItem1(int const itemIn){
+	item=itemIn;
+}
---
+++ LinkedList Controller/dataDTK.h
@@ -0,0 +1,9 @@
+#pragma once
+
+class DataDTK{
+public:
+	int getItem1() const;
+	void setItem1(int const itemIn);
+private:
+	int item;
+};
